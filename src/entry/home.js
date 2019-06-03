bd98a53645f6d2a9cd4a5a867c7e213fe0d94ce4
xiaopf <326258842@qq.com>
2019-06-04 00:24:06 +0800

    梳理页面细节

--- src/entry/home.js
+++ src/entry/home.js
@@ -1,1 +1,6 @@
-console.log('webpack 打包！')
+$(document).ready(function () {
+    $('.upComingEvents').click(function(){
+        console.log('webpack 打包！')
+        window.location.href = window.location.origin + '/events'
+    })
+})
